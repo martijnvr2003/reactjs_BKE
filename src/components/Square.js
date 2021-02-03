bbbe2e2d526d5439e3d88d793663534b6a366209
Martijn van Rooijen <534599@student.glu.nl>
2021-02-03 09:05:08 +0100

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+import React from "react";
+const square = ({ value, onClick}) => {
+const style = value ? `squares ${value}`:`squares`
+    return (
+        <button className={style} onClick={onClick}>
+    {value}
+    </button>
+    )
+}
+export default square;
